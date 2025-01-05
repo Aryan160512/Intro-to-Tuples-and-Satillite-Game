2c6cbf0160bdda50dade6dc7edd7d7e9ada547fe
Aryan160512 <157271425+Aryan160512@users.noreply.github.com>
2025-01-05 10:42:47 +0000

    Add files via upload

--- CW - The Satillite Game.py
+++ CW - The Satillite Game.py
@@ -1,5 +1,6 @@
 import pgzrun
 import random
+from time import time
 
 WIDTH = 800
 HEIGHT = 584
@@ -9,13 +10,34 @@ satillites = []
 nextSatillites = 0
 numOfSatillites = 10
 
+startTime = 0
+totalTime = 0
+
+lines = []
+
 for i in range(numOfSatillites):
     satillite = Actor('satillite')
     satillite.pos = (random.randint(50, 750), random.randint(50, 534))
     satillites.append(satillite)
 
+startTime = time()
+
+def on_mouse_down(pos):
+    global lines, nextSatillites
+
+    if nextSatillites < numOfSatillites:
+        if satillites[nextSatillites].collidepoint(pos):
+            if nextSatillites:
+                lines.append((satillites[nextSatillites - 1].pos, satillites[nextSatillites].pos))
+            
+            nextSatillites += 1
+        
+        else:
+            lines.clear()
+            nextSatillites = 0
+
 def draw():
-    global satillites
+    global satillites, totalTime
     screen.clear()
     screen.blit('spacebg', (0, 0))
 
@@ -23,6 +45,15 @@ def draw():
 
     for actor in satillites:
         actor.draw()
-        screen.draw.text(f'{number}', (actor.pos[0] - 30, actor.pos[1] + 30))
+        screen.draw.text(f'{number}', (actor.pos[0] - 20, actor.pos[1] + 20))
         number += 1
+
+    for l in lines:
+        screen.draw.line(l[0], l[1], (255, 255, 255))
+
+    if nextSatillites < numOfSatillites:
+        totalTime = time() - startTime
+        screen.draw.text(f'Time taken: {round(totalTime, 1)} seconds', (50, 50))
+    else:
+        screen.draw.text(f'Time taken to complete connections: {round(totalTime, 1)} seconds', (50, 50))
 pgzrun.go()
